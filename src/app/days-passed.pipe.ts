33ee9c50012dd423f4a9bbbdd91bbeed2ada9fb0
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-04-29 16:30:04 +0300

    Add a custom pipe to show the number of days that have passed since the quote was posted

--- src/app/days-passed.pipe.ts
+++ src/app/days-passed.pipe.ts
@@ -5,8 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DaysPassedPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: any): number {
+    let today:Date = new Date();
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    var dateDifference = Math.abs(todayWithNoTime-value);
+    const secondsInDay = 86400;
+    var dateDifferenceSeconds = dateDifference*0.001;
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
+    return dateCounter;
   }
 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,7 +10,7 @@ export class QuoteComponent implements OnInit {
 
   quotes = [
     new Quote(1,'It is better to be feared than loved, if you cannot be both.','Niccolo Machiavelli',88,9,new Date(2017,8,22)),
-    new Quote(2,'The only true wisdom is in knowing you know nothing.','Ki',8,9,new Date(2017,7,22))
+    new Quote(2,'The only true wisdom is in knowing you know nothing.','Ki',8,9,new Date(2019,3,28))
   ]
 
   completeQuote(isComplete, index){
