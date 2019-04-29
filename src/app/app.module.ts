f27ee0eddc255512e8631c0b850d29c648e0cb2a
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-04-29 17:16:46 +0300

    Add a form component to be used to add quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { DaysPassedPipe } from './days-passed.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    DaysPassedPipe
+    DaysPassedPipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule
