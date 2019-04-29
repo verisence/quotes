bb578619063221f7a5972a50c67c0f56b302d52e
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-04-29 13:59:50 +0300

    Add a component to show the quotes details such as the author and the votes. This can later be used to toggle the details on and off.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,8 +9,8 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new Quote(1,'phil','phil',8,9,new Date(2019,8,22)),
-    new Quote(2,'phil','Ki',8,9,new Date(2019,7,22))
+    new Quote(1,'It is better to be feared than loved, if you cannot be both.','Niccolo Machiavelli',8,9,new Date(2019,8,22)),
+    new Quote(2,'The only true wisdom is in knowing you know nothing.','Ki',8,9,new Date(2019,7,22))
   ]
 
   constructor() { }
