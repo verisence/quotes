da9a1fe3726ffdb6d99a88ceb7b43c964e26a5f4
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-04-30 08:45:13 +0300

    Add buttons for upvotes and downvotes.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote'
-import { globalAgent } from 'http';
 
 @Component({
   selector: 'app-quote',
@@ -10,8 +9,8 @@ import { globalAgent } from 'http';
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new Quote(1,'It is better to be feared than loved, if you cannot be both.','Niccolo Machiavelli',88,9,new Date(2004,8,22)),
-    new Quote(2,'The only true wisdom is in knowing you know nothing.','Ki',8,9,new Date(2019,3,29))
+    new Quote(1,'It is better to be feared than loved, if you cannot be both','Niccolo Machiavelli',88,9,new Date(2018,8,22)),
+    new Quote(2,'The only true wisdom is in knowing you know nothing','Clara Blair',8,9,new Date(2019,3,22))
   ]
 
   addNewQuote(quote){
