417c2b36ae31c2d43861772d2dc60e0f4a8e142f
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-04-30 09:22:03 +0300

    Add new quotes to the quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,7 +10,11 @@ export class QuoteComponent implements OnInit {
 
   quotes = [
     new Quote(1,'It is better to be feared than loved, if you cannot be both','Niccolo Machiavelli',88,9,new Date(2018,8,22)),
-    new Quote(2,'The only true wisdom is in knowing you know nothing','Clara Blair',8,9,new Date(2019,3,22))
+    new Quote(2,'The only true wisdom is in knowing you know nothing','Clara Blair',8,9,new Date(2019,3,22)),
+    new Quote(2,'Be yourself; everyone else is already taken.','Oscar Wilde',8,9,new Date(2019,2,22)),
+    new Quote(2,'Be the change that you wish to see in the world.','Mahatma Gandhi',8,9,new Date(2019,3,10)),
+    new Quote(2,'Without music, life would be a mistake.','Friedrich Nietzsche',8,9,new Date(2019,2,20)),
+    new Quote(2,'Imperfection is beauty, madness is genius and its better to be absolutely ridiculous than absolutely boring.','Marilyn Monroe',8,9,new Date(2018,0,22))
   ]
 
   addNewQuote(quote){
